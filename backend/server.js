90e1f01a67098fde5a5bebeb99366cc4ac8326a0
Ghost_Protocol <das05swpna@gmail.com>
2025-06-12 21:11:37 +0530

    creating a full stack app

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express= require('express')
+const mongoose=require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+
+
+const app=express();
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+// app.get('/',(req,res)=>{
+// res.json({mssg:"welcome"})
+// })
+app.use('/api/workouts',workoutRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+   app.listen(process.env.PORT,()=>{
+    console.log('listening on port 4000');
+})
+}).catch((error)=>{
+    console.log(error)
+})
+
