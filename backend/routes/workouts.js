0dd125082b5c6153b15121040c9a2dd23de9bc58
Ghost_Protocol <das05swpna@gmail.com>
2025-06-13 16:32:01 +0530

    mongodb schema was added

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const Workout=require('../models/workoutModel')
 const router =express.Router()
 router.get('/',(req,res)=>{
     res.json({mssg:"GET all workouts"})
@@ -6,8 +7,15 @@ router.get('/',(req,res)=>{
 router.get('/:id',(req,res)=>{
     res.json({mssg:'get a single workout'})
 })
-router.post('/',(req,res)=>{
-    res.json({mssg:'POST a new workout'})
+router.post('/',async (req,res)=>{
+    const {title,load,reps}=req.body
+    try{
+        const workout = await Workout.create({title,load,reps})
+        res.status(200).json(workout)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+   
 
 })
 router.delete('/:id',(req,res)=>{
