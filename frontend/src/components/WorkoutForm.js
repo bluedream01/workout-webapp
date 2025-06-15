02c16381056fe1b09ae45c245030f65324f7a643
Ghost_Protocol <das05swpna@gmail.com>
2025-06-15 12:57:03 +0530

    added bit of error handling

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
     const[load,setLoad]=useState('');
     const[reps,setReps]=useState('');
     const[error,setError]=useState(null);
+    const[emptyFields,setEmptyFields]=useState([])
     const handleSubmit = async (e) =>{
         e.preventDefault()
         const workout = {title,load,reps}
@@ -21,12 +22,14 @@ const WorkoutForm = () => {
         const json=await response.json()
         if(!response.ok){
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
           if(response.ok){
             setTitle('')
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
             console.log('new workout added',json)
             dispatch({type:'CREATE_WORKOUT',payload: json})
         }
@@ -41,6 +44,7 @@ const WorkoutForm = () => {
                 setTitle(e.target.value)
             }}
             value={title}
+            className={emptyFields.includes('title')? 'error':''}
             />
 
             <label>Load (in kg):</label>
@@ -50,6 +54,7 @@ const WorkoutForm = () => {
                 setLoad(e.target.value)
             }}
             value={load}
+            className={emptyFields.includes('load')? 'error':''}
             />
 
              <label>Reps:</label>
@@ -59,6 +64,7 @@ const WorkoutForm = () => {
                 setReps(e.target.value)
             }}
             value={reps}
+            className={emptyFields.includes('reps')? 'error':''}
             />
             <button>Add Workout</button>
             {error&& <div className="error">{error}</div>}
